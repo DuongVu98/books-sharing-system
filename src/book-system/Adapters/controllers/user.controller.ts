1c0ffdbf2132b99d4d62d0f52ddff2472893f0dc
Vu Tung Duong <duong.vu@bigin.vn>
2019-12-21 00:49:10 +0700

    add books-list controller

--- src/book-system/Adapters/controllers/user.controller.ts
+++ src/book-system/Adapters/controllers/user.controller.ts
@@ -2,42 +2,46 @@ import { Controller, Get, Param } from "@nestjs/common";
 import { FindBookInteractor } from "src/book-system/Usecases/interactors/find-book.interactor";
 import { GetBookDetailInteractor } from "src/book-system/Usecases/interactors/get-book-detail.interactor";
 import { GetUserProfileInteractor } from "src/book-system/Usecases/interactors/get-user-profile.interactor";
-import { Crud, CrudController } from '@nestjsx/crud';
+import { Crud, CrudController } from "@nestjsx/crud";
 import { UserService } from "src/book-system/Domains/repositories/user.service";
 import { User } from "src/book-system/Domains/entities/user.entity";
 
 @Crud({
     model: {
-        type: User
-    }
+        type: User,
+    },
 })
-@Controller('api/user')
-export class UserController implements CrudController<User>{
-
+@Controller("api/user")
+export class UserController implements CrudController<User> {
     constructor(
         public service: UserService,
         private readonly findBookInteractor: FindBookInteractor,
         private readonly getBookDetailInteractor: GetBookDetailInteractor,
-        private readonly getUserProfileInteractor: GetUserProfileInteractor
-    ){}
+        private readonly getUserProfileInteractor: GetUserProfileInteractor,
+    ) {}
 
     @Get()
-    userHome(){
-        return 'this is user api';
+    userHome() {
+        return "this is user api";
     }
 
-    @Get('search/:search')
-    findBooksBySearching(@Param('search') searchString: string){
+    @Get("books-list")
+    getBookListForHomePage(){
+        return this.findBookInteractor.getAllBooks();
+    }
+
+    @Get("search/:search")
+    findBooksBySearching(@Param("search") searchString: string) {
         return this.findBookInteractor.getBooksBySearchString(searchString);
     }
 
-    @Get('book/:id')
-    getBookDetail(@Param('id') id: string){
+    @Get("book/:id")
+    getBookDetail(@Param("id") id: string) {
         return this.getBookDetailInteractor.getBookById(id);
     }
 
-    @Get('profile/:userId')
-    getUserProfileDetail(@Param('userId') id: string){
+    @Get("profile/:userId")
+    getUserProfileDetail(@Param("userId") id: string) {
         return this.getUserProfileInteractor.getUserById(id);
     }
 }
